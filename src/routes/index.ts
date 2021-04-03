90dabfbac105febf847f7f0d0b78934408653a0f
otallyto <rodrigues.tallyto@hotmail.com>
2021-04-03 19:25:02 -0300

    wip: rota não encontrada

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,8 +3,11 @@ import {Router} from 'express'
 const router = Router()
 
 router.get('/', (req, res)=>{
-  console.log(req)
   res.send("<h1>Seja Bem Vindo ao Meu Site!</h1>")
 })
+
+router.get('*', (req, res)=> {
+  res.send('<h1>Página não encontrada :(</h1>')
+} )
 
 export default router
